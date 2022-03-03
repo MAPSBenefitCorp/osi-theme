e2c5418240a694f0c5b1845ffc9f122ed8f3b0dc
Jeet Saha <soumyajit@wpthemers.net>
2022-03-03 22:25:00 +0530

    finalize styles

--- includes/inc-theme-setup.php
+++ includes/inc-theme-setup.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Remove twentytwentytwo hooks
+ */
+remove_action('wp_head', 'twentytwentytwo_preload_webfonts');
+remove_action('admin_init', 'twentytwentytwo_editor_styles');
+
+/**
  * Enqueue child theme CSS/JS
  */
 add_action('wp_enqueue_scripts', function () {
@@ -20,4 +26,30 @@ add_filter('excerpt_length', function ($length) {
 
 add_filter('excerpt_more', function ($more) {
     return '...';
+});
+
+/**
+ * Author Link Shortcode
+ */
+add_shortcode('post_author', function () {
+    // get the global post
+    global $post;
+
+    // post id
+    $post_id = $post->ID;
+
+    // post author id
+    $author_id = $post->post_author;
+
+    echo $post_id;
+
+    // author name
+    $author_name = get_the_author_meta('display_name', $author_id);
+
+    // author link
+    $author_link = get_author_posts_url($author_id);
+
+    // return link
+    return '<a href="' . $author_link . '">' . $author_name . '</a>';
+
 });
